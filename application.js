209e9951e9c82ceb0907da0b2d9dadece672f52f
Norman Joyner <norman.joyner@gmail.com>
2017-03-12 12:54:37 -0400

    Fix plugin configuration
    Ensure core exists before accessing its properties

--- application.js
+++ application.js
@@ -14,6 +14,60 @@ module.exports = new ContainershipPlugin({
     type: ['core', 'cli'],
     name: 'cloud',
 
+    runCLI: function() {
+        let commands = _.map(cli, function(configuration, command) {
+            configuration.name = command;
+            return configuration;
+        });
+
+        return {
+            commands: commands,
+            middleware: [
+                function(options, fn) {
+                    if(options.url.indexOf(constants.environment.CLOUD_API_BASE_URL) == 0) {
+                        let original_url = options.url;
+                        options.url = [
+                            constants.environment.CLOUD_API_BASE_URL,
+                            'v2',
+                            'organizations',
+                            options.headers['x-containership-cloud-organization'],
+                            'clusters',
+                            options.headers['x-containership-cloud-cluster'],
+                            'proxy'
+                        ].join('/');
+
+                        let original_method = options.method;
+                        options.method = 'POST';
+
+                        options.headers = _.pick(options.headers, [
+                            'authorization'
+                        ]);
+
+                        let original_qs = options.qs;
+                        options.qs = {};
+
+                        let original_body = options.json;
+
+                        let proxy_url = url.parse(original_url).path.split('/');
+                        proxy_url.splice(1, 1);
+
+                        options.json = {
+                            url: proxy_url.join('/'),
+                            qs: original_qs,
+                            method: original_method
+                        };
+
+                        if((original_method == 'POST' || original_method == 'PUT') && !_.isUndefined(original_body)) {
+                            options.json.data = original_body;
+                        }
+                    }
+
+                    return fn();
+                }
+            ]
+        };
+    },
+
     runLeader: function(core) {
         module.exports._initialize(core, leader.initialize);
     },
@@ -23,84 +77,35 @@ module.exports = new ContainershipPlugin({
     },
 
     initialize: function(core) {
-        if(core.options.mode === 'leader') {
+        if(!core || !core.logger) {
+            return module.exports.runCLI();
+        } else if(core.options.mode === 'leader') {
             return module.exports.runLeader(core);
+        } else if(core.options.mode === 'follower') {
+            return module.exports.runFollower(core);
+        } else if(core.logger) {
+            core.logger.register('containership-cloud');
+            core.loggers['containership-cloud'].log('error', 'Invalid configuration found when initializing containership cloud plugin!');
         }
-
-        return module.exports.runFollower(core);
     },
 
     _initialize: function(core, initialize) {
-        if(_.has(core, 'logger')) {
-            core.logger.register('containership-cloud');
+        core.logger.register('containership-cloud');
 
-            let config = this.get_config('core');
+        let config = this.get_config('core');
 
-            cluster_discovery.discover(core.cluster_id || core.options.cluster_id, config, function(err, cidr) {
-                if(err) {
-                    core.loggers['containership-cloud'].log('error', err.message);
-                } else {
-                    core.loggers['containership-cloud'].log('debug', `Discovering peers: ${cidr.join(', ')}`);
+        cluster_discovery.discover(core.cluster_id || core.options.cluster_id, config, function(err, cidr) {
+            if(err) {
+                core.loggers['containership-cloud'].log('error', err.message);
+            } else {
+                core.loggers['containership-cloud'].log('debug', `Discovering peers: ${cidr.join(', ')}`);
 
-                    core.cluster.legiond.options.network.cidr = cidr;
-                    core.cluster.legiond.actions.discover_peers(cidr);
-                }
+                core.cluster.legiond.options.network.cidr = cidr;
+                core.cluster.legiond.actions.discover_peers(cidr);
+            }
 
-                initialize(core, config);
-            });
-        } else {
-            let commands = _.map(cli, function(configuration, command) {
-                configuration.name = command;
-                return configuration;
-            });
-
-            return {
-                commands: commands,
-                middleware: [
-                    function(options, fn) {
-                        if(options.url.indexOf(constants.environment.CLOUD_API_BASE_URL) == 0) {
-                            let original_url = options.url;
-                            options.url = [
-                                constants.environment.CLOUD_API_BASE_URL,
-                                'v2',
-                                'organizations',
-                                options.headers['x-containership-cloud-organization'],
-                                'clusters',
-                                options.headers['x-containership-cloud-cluster'],
-                                'proxy'
-                            ].join('/');
-
-                            let original_method = options.method;
-                            options.method = 'POST';
-
-                            options.headers = _.pick(options.headers, [
-                                'authorization'
-                            ]);
-
-                            let original_qs = options.qs;
-                            options.qs = {};
-
-                            let original_body = options.json;
-
-                            let proxy_url = url.parse(original_url).path.split('/');
-                            proxy_url.splice(1, 1);
-
-                            options.json = {
-                                url: proxy_url.join('/'),
-                                qs: original_qs,
-                                method: original_method
-                            };
-
-                            if((original_method == 'POST' || original_method == 'PUT') && !_.isUndefined(original_body)) {
-                                options.json.data = original_body;
-                            }
-                        }
-
-                        return fn();
-                    }
-                ]
-            };
-        }
+            initialize(core, config);
+        });
     },
 
     reload: function() {}
